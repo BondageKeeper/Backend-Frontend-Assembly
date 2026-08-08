2040736adc074ef7295e845ef657a76a221cfb42
Sergey <rychalovser@gmail.com>
2026-08-08 22:04:40 +0300

    Create instance_password_page.py
    It will be modified over time

---
+++ Register_page_project/Backend/app/instance_password_page.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel , Field , field_validator
+from fastapi import status
+
+class TableFrameworkCO(BaseModel):
+    nickname: str = Field(min_length=2,max_length=20)
+    email: str
+    password: str
+    @field_validator("email")
+    @classmethod
+    def check_domen(cls,email: str):
+        import re
+        if not re.findall(r'[\w\-.]+@(?:gmail\.com|yandex\.ru)',email):
+            raise ValueError('Please , use yandex.ru or gmail.com domens!')
+        return email
+
+class PresentCredentials(BaseModel):
+    nickname: str = Field(min_length=2,max_length=20)
+    password: str
