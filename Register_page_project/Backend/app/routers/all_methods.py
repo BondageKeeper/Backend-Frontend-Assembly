eabe786f66fb0fa7a259aeb34b9f4a4a2ab9946e
Sergey <rychalovser@gmail.com>
2026-08-10 17:16:10 +0300

    Update all_methods.py
    Here I added a new route for sign-in part and even embedded links to hash-functions

--- Register_page_project/Backend/app/routers/all_methods.py
+++ Register_page_project/Backend/app/routers/all_methods.py
@@ -1,18 +1,30 @@
 from fastapi import HTTPException , APIRouter , status , Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from Backend_password_page.app.crud_password_page import keep_open_database
-from Backend_password_page.app.instance_password_page import TableFrameworkCO , PresentCredentials
+from Backend_password_page.app.instance_password_page import TableFrameworkCO , TableSignInCO
 from Backend_password_page.app import crud_password_page
+from Backend_password_page.app.hashed_password import hash_password , verify_password
+
 router = APIRouter(prefix='/password_page')
 
 @router.post("/create_password",status_code=status.HTTP_201_CREATED)
 async def create_password(instance: TableFrameworkCO,session: AsyncSession = Depends(keep_open_database)):
-    new_password = await crud_password_page.create_password(instance=instance,session=session)
-    return new_password
+    user_dict = instance.model_dump()
+    user_dict["password"] = hash_password(user_dict["password"])
+    new_user = await crud_password_page.create_password(instance=TableFrameworkCO(**user_dict),session=session)
+    return {"status":"success","user":user_dict["nickname"]}
 
-@router.post("/return_to_account",status_code=status.HTTP_201_CREATED)
-async def return_to_account(instance: PresentCredentials,session: AsyncSession = Depends(keep_open_database)):
-    checking = await crud_password_page.return_to_account(instance=instance,session=session)
-
+@router.post("/sign-in")
+async def sign_in(instance: TableSignInCO,session: AsyncSession = Depends(keep_open_database)):
+     user_email = instance.email
+     found_email = await crud_password_page.get_user_email(user_email,session)
+     if found_email is None:
+         raise HTTPException(status_code=404,detail="Email was not found!")
+     else:
+         written_parameters = instance.model_dump()
+         if not verify_password(written_parameters["password"],found_email.password):
+              raise HTTPException(status_code=401,detail="Wrong Password!")
+         else:
+             return {"status":"Welcome","user":found_email.nickname}
 
 
