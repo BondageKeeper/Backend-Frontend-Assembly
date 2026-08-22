2d69798b28610f5c1acef0e142fc5e61719dc2aa
Sergey <rychalovser@gmail.com>
2026-08-22 19:59:02 +0300

    Create crud.py

---
+++ Cards-Hub-Project/Backend/app/crud.py
@@ -0,0 +1,75 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession , create_async_engine , async_sessionmaker
+from sqlalchemy.orm import  Mapped , mapped_column , DeclarativeBase
+from datetime import datetime
+from Backend_planner.app.instance_password_page import TableFrameworkCO , TableCreateCardsCO
+from sqlalchemy import select , ForeignKey , JSON
+
+class AlchemyLaunch(DeclarativeBase):
+    pass
+
+class TableFrameworkDB(AlchemyLaunch):
+    __tablename__ = 'password_table'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nickname: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    created_at: Mapped[str] = mapped_column(default=lambda: datetime.now().strftime("%d.%m.%Y %H:%M"))
+
+class TableCreateCardsDB(AlchemyLaunch):
+    __tablename__ = 'cards_table'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_email: Mapped[str] = mapped_column(ForeignKey('password_table.email',ondelete='CASCADE'))
+    todo_column: Mapped[list[str]] = mapped_column(JSON,default=list)
+    progress_column: Mapped[list[str]] = mapped_column(JSON,default=list)
+    done_column: Mapped[list[str]] = mapped_column(JSON,default=list)
+
+URL = 'postgresql+asyncpg://postgres:0631@localhost:5432/password_database'
+engine_origin = create_async_engine(URL,echo=True)
+session_origin = async_sessionmaker(engine_origin,expire_on_commit=False)
+
+async def keep_open_database():
+    async with session_origin() as session:
+        yield session
+
+async def create_password(instance: TableFrameworkCO,session: AsyncSession):
+    validated_instance = TableFrameworkDB(nickname=instance.nickname,
+                                          email=instance.email,
+                                          password=instance.password
+                                          )
+    session.add_all([validated_instance])
+    await session.commit()
+    return {
+        "status_code" : 201,
+        "comment" : "successful connection!"
+    }
+
+async def get_user_email(email: str,session: AsyncSession):
+    request = select(TableFrameworkDB).where(TableFrameworkDB.email == email)
+    result = await session.execute(request)
+    found_email = result.scalar_one_or_none()
+    if found_email is None:
+        return None
+    return found_email
+
+from sqlalchemy import update
+async def update_user_password(nickname: str,new_hash: str,session: AsyncSession):
+    request = update(TableFrameworkDB).where(TableFrameworkDB.nickname == nickname).values(password=new_hash)
+    await session.execute(request)
+    await session.commit()
+
+async def store_cards(instance: TableCreateCardsCO,session: AsyncSession):
+    validated_cards = TableCreateCardsDB(
+                          user_email = instance.user_email,
+                          todo_column = instance.todo_column,
+                          progress_column = instance.progress_column,
+                          done_column = instance.done_column
+                                        )
+    session.add_all([validated_cards])
+    await session.commit()
+    return {
+        "status_code" : 201,
+        "comment": "successful connection!"
+    }
+
+
